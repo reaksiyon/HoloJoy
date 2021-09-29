16f0867a5a47135ebca89e46d806dc1db351f6e6
Emir Nazik <spearkinq@gmail.com>
2021-09-29 03:16:17 +0300

    Revert "Initialization"
    This reverts commit f12c77ca1d41767f899ae6dff28297ab523475eb.

--- Assets/Scripts/IProjectile.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-public interface IProjectile
-{
-    IEntity Owner { get; }
-
-    void CreateProjectile(IEntity owner, Vector3 direction);
-}
